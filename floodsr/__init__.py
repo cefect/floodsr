b7cee4f16c9ca4e2fb4f1576c925eb7cc6191047
cefect <bryant.seth@gmail.com>
2026-02-21 19:36:35 -0700

    add versiion detection

--- floodsr/__init__.py
+++ floodsr/__init__.py
@@ -1,2 +1,9 @@
 """floodsr package."""
 
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    # Read installed package metadata so version stays tied to pyproject.toml.
+    __version__ = version("floodsr")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
